50d57c37b048f2262ea6613a10f7d27ca2a291ad
Raissa Rodrigues <raissarodrigues.ufjf@gmail.com>
2023-12-07 10:58:42 -0300

    melhorando o form

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ document.entrada.addEventListener('submit', leFormulario);
 
 const olJoao = document.querySelector("#olJoao");
 const olMaria = document.querySelector("#olMaria");
+
+function leFormulario(event){
+    event.preventDefault();
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
+
+    console.log(`fruta: ${fruta} qtd: ${quantidade}`);
+
+    //document.entrada.submit();
+}
