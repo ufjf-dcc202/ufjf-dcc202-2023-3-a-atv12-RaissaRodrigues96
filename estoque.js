ecf51116014149d21d604f7c6f23b8bd79845959
Raissa Rodrigues <raissarodrigues.ufjf@gmail.com>
2023-12-14 09:29:35 -0300

    altera e cria função

--- estoque.js
+++ estoque.js
@@ -57,42 +57,43 @@ export function transacaoNoEstoque(origem, destino, tipo, quantidade){
       
       return;
   }
-        
-    }
-}
 
-export function dePomarParaPessoa(destino, quantidade, fruta){
-    const pessoa = estoque[destino];
-    let monte;
-    for(let i = 0; i < pessoa.length; i++){
-        if(pessoa[i].tipo === fruta){
-            monte = pessoa[i];
-            break;
+  else{
+        const itemOrigem = estoque[origem].find(item => item.tipo === tipo);
+        const itemDestino = estoque[destino].find(item => item.tipo === tipo);
+
+        if(!itemOrigem)
+        {
+            return;
+        }
+
+        else if(quantidade > itemOrigem.quantidade)
+        {
+            if(itemDestino)
+            {
+                itemDestino.quantidade += itemOrigem.quantidade;
+            }else
+            {
+                estoque[destino].push({tipo: tipo, quantidade: itemOrigem.quantidade});
+            }
+            itemOrigem.quantidade = 0;
+        }
+
+        else 
+        {
+            if(itemDestino)
+            {
+                itemDestino.quantidade += quantidade;
+            }else
+            {
+                estoque[destino].push({tipo, quantidade});
+            }
+            itemOrigem.quantidade -= quantidade;
         }
     }
     
-    if(!monte){
-        monte = { tipo: fruta, qtd: 0 }; 
-        pessoa.push(monte);
-    }
-    monte.qtd += quantidade;
+    return;
 }
+  
 
-export function dePessoaParaPomar(origem, quantidade, fruta){
-    const pessoa = estoque[origem];
-    let monte;
-    for(let i = 0; i < pessoa.length; i++){
-        if(pessoa[i].tipo === fruta){
-            monte = pessoa[i];
-            break;
-        }
-    }
-
-    if(!monte){
-        return;
-    }
-    monte.qtd -= Math.min(quantidade, monte.qtd);
-}
-
-
-//export { getEstoque };
+//export { getEstoque, limpaEstoque, transacaoNoEstoque };
