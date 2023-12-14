7f2f2ae5d47585ee2a2ac683a3b7eb49b7cae6a7
Raissa Rodrigues <raissarodrigues.ufjf@gmail.com>
2023-12-14 09:10:53 -0300

    alteração na main

--- main.js
+++ main.js
@@ -47,11 +47,11 @@ function preencheListaPessoa(pessoa, lista){
     lista.innerHTML = "";
 
     if(Array.isArray(pessoa)) 
-    
+
     for(let i = 0; i < pessoa.length; i++){
         const monte = pessoa[i];
         const eLi = document.createElement('li');
-        eLi.innerText = `${monte.tipo}: ${monte.qtd}`;
+        eLi.innerText = `${monte.tipo}: ${monte.quantidade}`;
         lista.append(eLi);
     }
 }
